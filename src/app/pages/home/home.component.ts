e6d053aa0a79cbe90948e7e0d52e82cc1101a439
framirez91 <framirez.d85@gmail.com>
2022-04-30 11:22:17 -0400

    CAMBIO DE API A MARVEL

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,54 +1,47 @@
 import { Component, OnInit } from '@angular/core';
-import { Movie } from 'src/app/interfaces/interfaces';
+import { Result, Thumbnail,Data } from 'src/app/interfaces/interfaces';
 import { MoviesService } from 'src/app/services/movies.service';
-
+import { Observable, observable } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-   movielist:Movie[] = [];
-   textSearch:string = "";
-   loading:boolean = false;
-
-
-  constructor(private service:MoviesService) { }
-
-  ngOnInit(): void {
-    this.service.getDataMovies()
-    .subscribe(resp=>{
-      console.log(resp.Search)
-      this.movielist = resp.Search;
-    })
-  }
+  characters: Result[] = [];
+  textSearch: string = '';
+  loading: boolean = false;
+  thumbnailList: string = '';
   
-  onClickSearch(){
+
+  constructor(private service: MoviesService) {}
+  ngOnInit(): void {  
     this.loading = true;
-    console.log("click en busqueda:"+this.textSearch);
-    
-    setTimeout(()=>{
-      this.service.searchDataMovie(this.textSearch)
-    .subscribe(resp=>{
-      this.loading = false;
-      console.log(resp.Search);
-      if(resp.Search){
-        this.movielist = resp.Search;
-      }else{
-        this.movielist = [];
-
-      }
-    })
-    },2500);
-
-    
+    setTimeout(() => {
+      this.service.getAllcharacters().subscribe((resp) => {
+        this.loading = false;
+        this.characters = resp.data.results;
+        console.log(this.characters);
+      })
+    }, 1);
   }
 
-      
-  
-  
-  
+  onClickSearch() {
+    console.log('busqueda');
+    this.loading = true;
+    console.log('click en busqueda:' + this.textSearch);
 
-
-
+    setTimeout(() => {
+    this.service.searchCharacter(this.textSearch).subscribe((resp) => {
+        this.loading = false;
+        console.log(resp);
+        if (resp.data.results.length > 0) {
+          
+          this.characters = resp.data.results;
+        } else {
+          this.characters = [];
+        }
+      });
+    }, 2500);
+  }
 }
--- src/app/services/movies.service.ts
+++ src/app/services/movies.service.ts
@@ -1,6 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { ApiResult } from '../interfaces/interfaces';
+import { Result,Thumbnail,Data, RootObject } from '../interfaces/interfaces';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -8,14 +10,27 @@ import { ApiResult } from '../interfaces/interfaces';
 export class MoviesService {
 
   constructor(private http:HttpClient) { }
-   
-    getDataMovies(){
-      return this.http.get<ApiResult>('https://www.omdbapi.com/?s=harry&apikey=d4d9c712');
+    PUBLIC_KEY= '8025e3d0f1c5672eb6f9bc796b613b82';
+    HASH= '5e8003b410af946e656b6988ed048af7';
+    URL_API='https://gateway.marvel.com/v1/public/characters?nameStartsWith=i&ts=1000&apikey='+this.PUBLIC_KEY+'&hash='+this.HASH;
+
+    getAllcharacters(): Observable<any> {
+      return this.http.get<Result>(this.URL_API)
     }
 
-    searchDataMovie(text:string){
-      return this.http.get<ApiResult>('https://www.omdbapi.com/?s='+text+'&apikey=d4d9c712');
+
+    getThumbnail(){
+      return this.http.get<Thumbnail>(this.URL_API)
+      
     }
+
+    searchCharacter(text:string){
+      return this.http.get<RootObject>('https://gateway.marvel.com/v1/public/characters?nameStartsWith='+text+'&ts=1000&apikey=8025e3d0f1c5672eb6f9bc796b613b82&hash=5e8003b410af946e656b6988ed048af7');
+    }
+                              
+
+
+    
 
 
 
